8a93577c497ec020154d63e4c057b5480d9f32ff
Junes-PhD <asowa42@gmail.com>
2022-04-28 22:55:23 -0500

    manual melee combat working

--- dice.py
+++ dice.py
@@ -121,6 +121,7 @@ def shooting_phase(attacker, defender):
     #print(damage)
     return(damage)
 
+#not working
 def melee_phase_brute_force_try(attacker, defender):
     
     attack_dice = dice_check(attacker['WA'], attacker['WS'])
@@ -261,136 +262,366 @@ def melee_phase_brute_force_try(attacker, defender):
     print()
     print(str(res)) 
 
+#not working
 def melee_phase_algo1(attacker, defender):
-    
+
+    def zero_dice():
+        print('zero dice')
+        
+        nonlocal status
+
+        status['ah'] = 0
+        status['ac'] = 0
+        status['atd'] = 0
+        status['dh'] = 0
+        status['dc'] = 0
+        status['dtd'] = 0
+
+    def tally_leftover():
+        print('tally leftover')
+
+        nonlocal status
+        nonlocal damage_totals
+        attacker_damage = 0
+        defender_damage = 0
+
+        #add up leftover dice
+        attacker_damage += status['ah'] + status['ahd']
+        attacker_damage += status['ac'] + status['acd']
+        defender_damage += status['dh'] + status['dhd']
+        defender_damage += status['dc'] + status['dcd']
+        #damage_totals[0] = (status['ad'],status['dd'])
+        #zero_dice()
+        return(attacker_damage,defender_damage)
+
+    def all_parries_dmg(): 
+        print('guaranteed dmg')
+        
+        nonlocal status
+        attacker_damage = 0
+        defender_damage = 0
+
+        crit_dif = status['ac'] - status['dc']
+        hit_dif = status['ah'] - status['dh']
+        
+        #use up crits to parry hits
+        if (crit_dif < 0 and hit_dif > 0) or (crit_dif > 0 and hit_dif < 0):
+            if abs(crit_dif) > abs(hit_dif):
+                crit_dif = crit_dif + hit_dif
+                hit_dif = 0
+            else:
+                hit_dif = crit_dif + hit_dif
+                crit_dif = 0
+        
+        #assign damage based on polarity
+        if crit_dif >= 0:
+            attacker_damage +=  crit_dif * status['acd']
+        else:
+            defender_damage +=  abs(crit_dif) * status['dcd']
+        
+        if hit_dif >= 0:
+            attacker_damage +=  hit_dif * status['ahd']
+        else:
+            defender_damage +=  abs(hit_dif) * status['dhd']
+
+        return(attacker_damage,defender_damage)
+ 
+
+            
+
+
     #roll the dice
     attack_dice = dice_check(attacker['WA'], attacker['WS'])
     defense_dice = dice_check(defender['WA'], defender['WS'])
 
-    attacker_hits = attack_dice['Hits']
-    attacker_crits = attack_dice['Crits']
-    attacker_total_dice = attacker_hits + attacker_crits
+    #load stats
+    status = {}
+    status['ah'] = attack_dice['Hits']
+    status['ahd'] =  attacker['WD']
+    status['ac'] = attack_dice['Crits']
+    status['acd'] = attacker['WCD']
+    status['atd'] = status['ah'] + status['ac']
+    status['aw'] = attacker['W']
+    status['ad'] = 0 #attacker total damage done
             
-    defender_hits = defense_dice['Hits']
-    defender_crits = defense_dice['Crits']
-    defender_total_dice = defender_hits + defender_crits
+    status['dh'] = defense_dice['Hits']
+    status['dhd'] = defender['WD']
+    status['dc'] = defense_dice['Crits']
+    status['dcd'] = defender['WCD']
+    status['dtd'] = status['dh'] + status['dc']
+    status['dw'] = defender['W']
+    status['dd'] = 0 #defender total damage done
 
     #tally of each simulation
     damage_totals ={}
-    damage_pos_counter = 0
+    print(all_parries_dmg())
+    zero_dice()    
+
+    #1 atk parry hit 2 def parry hit
+    #3 atk parry crit 4 def parry crit
+    #5 atk use hit 6 def use hit
+    #7 atk use crit 8 def use crit
+    #9 dead
+    damage_sequence = 0
+
+    if status['atd'] == 0 or status['dtd'] == 0:
+        tally_leftover()
+    '''
+    while attacker_total_dice != 0 or defender_total_dice != 0:
+        #kill defender
+        if attacker_crits != 0 and defender_wounds <= attacker_crit_dmg:
+            attack_dmg += attacker_crit_dmg
+            defender_wounds = 0
+            zero_dice()
+        #kill defender
+        elif attacker_hits != 0 and defender_wounds <= attacker_hit_dmg:
+            attack_dmg += attacker_hit_dmg
+            defender_wounds = 0
+            zero_dice()
+        #get wounds on defender since death in guarantee
+    '''
+
+  
+def melee_phase_user_picks(attacker, defender):
+
+    def zero_dice():
+        #print('zero dice')
+        
+        nonlocal status
+
+        status['ah'] = 0
+        status['ac'] = 0
+        status['atd'] = 0
+        status['dh'] = 0
+        status['dc'] = 0
+        status['dtd'] = 0
+
+    def retotal_dice():
+         nonlocal status
+
+         status['atd'] = status['ah'] + status['ac']
+         status['dtd'] = status['dh'] + status['dc']
+
+    def print_update_text():
+        nonlocal status
+
+        if status['aw'] <= 0:
+            print('DEAD**',end='')
+        else:
+            print('      ',end='')
+        print('Attacker-',end='')
+        print(' Crits:',end='')
+        print(status['ac'],end='')
+        print(' Hits:',end='')
+        print(status['ah'],end='')
+        print(' Wounds:',end='')
+        print(status['aw'],end='')
+        print(' Damage Done:',end='')
+        print(status['ad'],end='')
+        
+        
+        print('       ',end='')
+        
+        
+        if status['dw'] <= 0:
+            print('DEAD**',end='')
+        else:
+            print('      ',end='')
+        print('Defender-',end='')
+        print(' Crits:',end='')
+        print(status['dc'],end='')
+        print(' Hits:',end='')
+        print(status['dh'],end='')
+        print(' Wounds:',end='')
+        print(status['dw'],end='')
+        print(' Damage Done:',end='')
+        print(status['dd'],end='')
+
+        print()
+
 
     def tally_leftover():
+
+        #print('tally leftover')
+
+        nonlocal status
+        attacker_damage = 0
+        defender_damage = 0
+
+        #add up leftover dice
+        attacker_damage += status['ah'] * status['ahd']
+        attacker_damage += status['ac'] * status['acd']
+        defender_damage += status['dh'] * status['dhd']
+        defender_damage += status['dc'] * status['dcd']
+
+        status['ad'] += attacker_damage
+        status['dd'] += defender_damage
+
+        status['dw']-= attacker_damage
+        status['aw'] -= defender_damage
+
+        zero_dice()
+        if  status['aw'] < 0:
+            status['aw'] = 0
+        if  status['dw'] < 0:
+            status['dw'] = 0
+
+    def get_decision():
+        print("Enter Decision:", end='')
+        usr_choice = input()
+
+        while usr_choice != 'p' and usr_choice != 'a' and usr_choice != 'attack' and usr_choice != 'parry':
+            print("Enter Decision:", end='')
+            usr_choice = input()
+
+        if usr_choice == 'p':
+            usr_choice = 'parry'
+        if usr_choice == 'a':
+            usr_choice = 'attack'
+        return(usr_choice)    
+
+
+    #roll the dice
+    attack_dice = dice_check(attacker['WA'], attacker['WS'])
+    defense_dice = dice_check(defender['WA'], defender['WS'])
+
+    #load stats
+    status = {}
+    status['ah'] = attack_dice['Hits']
+    status['ahd'] =  attacker['WD']
+    status['ac'] = attack_dice['Crits']
+    status['acd'] = attacker['WCD']
+    status['atd'] = status['ah'] + status['ac']
+    status['aw'] = attacker['W']
+    status['ad'] = 0 #attacker total damage done
+            
+    status['dh'] = defense_dice['Hits']
+    status['dhd'] = defender['WD']
+    status['dc'] = defense_dice['Crits']
+    status['dcd'] = defender['WCD']
+    status['dtd'] = status['dh'] + status['dc']
+    status['dw'] = defender['W']
+    status['dd'] = 0 #defender total damage done
+
+
+    if status['atd'] == 0 or status['dtd'] == 0:
+        tally_leftover()
+    
+    print_update_text()
+    while status['atd'] != 0 and status['dtd'] != 0:
+        print('Attacker ', end='')
+        if get_decision() == 'parry':
+            if status['ac'] != 0 and status['dc'] != 0:
+                status['ac'] -= 1
+                status['dc'] -= 1
+            elif status['ac'] != 0 and status['dh'] != 0:
+                status['ac'] -= 1
+                status['dh'] -= 1
+            elif status['ah'] != 0 and status['dh'] != 0:
+                status['ah'] -= 1
+                status['dh'] -= 1
+            elif status['ac'] != 0:
+                status['ad'] += status['acd']
+                status['ac'] -= 1
+                status['dw'] -= status['acd']
+            elif status['ah'] != 0:
+                status['ad'] += status['ahd']
+                status['ah'] -= 1
+                status['dw'] -= status['ahd']
+            else:
+                pass
+        else:
+            if status['ac'] != 0:
+                status['ad'] += status['acd']
+                status['ac'] -= 1
+                status['dw'] -= status['acd']
+            elif status['ah'] != 0:
+                status['ad'] += status['ahd']
+                status['ah'] -= 1
+                status['dw'] -= status['ahd']
+            else:
+                pass
+        if status['dw'] <= 0:
+            status['dw'] = 0
+            zero_dice()
+        retotal_dice()
+        print_update_text()
+
+
+        print('Defender ', end='')
+        if get_decision() == 'parry':
+            if status['dc'] != 0 and status['ac'] != 0:
+                status['dc'] -= 1
+                status['ac'] -= 1
+            elif status['dc'] != 0 and status['ah'] != 0:
+                status['dc'] -= 1
+                status['ah'] -= 1
+            elif status['dh'] != 0 and status['ah'] != 0:
+                status['dh'] -= 1
+                status['ah'] -= 1
+            elif status['dc'] != 0:
+                status['dd'] += status['dcd']
+                status['dc'] -= 1
+                status['aw'] -= status['dcd']
+            elif status['dh'] != 0:
+                status['dd'] += status['dhd']
+                status['dh'] -= 1
+                status['aw'] -= status['ahd']
+            else:
+                pass
+        else:
+            if status['dc'] != 0:
+                status['dd'] += status['dcd']
+                status['dc'] -= 1
+                status['aw'] -= status['dcd']
+            elif status['dh'] != 0:
+                status['dd'] += status['dhd']
+                status['dh'] -= 1
+                status['aw'] -= status['dhd']
+            else:
+                pass
+        if status['dw'] <= 0:
+            status['dw'] = 0
+            zero_dice()
+        retotal_dice()
+        print_update_text()
+        
+    tally_leftover()
+    print_update_text()        
+
+    '''
+    print("Attack Dice:", end='')
+    atk_num = input()
+  
+    print("Defense Dice:", end='')
+    def_num = input()
+
+    print(atk_num,def_num)
+    return(atk_num,def_num)
+
+
+    while attacker_total_dice != 0 or defender_total_dice != 0:
+        #kill defender
+        if attacker_crits != 0 and defender_wounds <= attacker_crit_dmg:
+            attack_dmg += attacker_crit_dmg
+            defender_wounds = 0
+            zero_dice()
+        #kill defender
+        elif attacker_hits != 0 and defender_wounds <= attacker_hit_dmg:
+            attack_dmg += attacker_hit_dmg
+            defender_wounds = 0
+            zero_dice()
+        #get wounds on defender since death in guarantee
+    '''
+    
         
 
-    for x in range(2**iterations_x):
-        for y in range(2**iterations_y):
-            print(x,y)
-
-            #reset stats for another calculation
-            attack_dmg = 0
-            defense_dmg = 0
-            
-            attacker_hits = attack_dice['Hits']
-            attacker_crits = attack_dice['Crits']
-            attacker_total_dice = attacker_hits + attacker_crits
-            
-            defender_hits = defense_dice['Hits']
-            defender_crits = defense_dice['Crits']
-            defender_total_dice = defender_hits + defender_crits
-
-            attacker_wounds = attacker['W']
-            defender_wounds = defender['W']
-
-            mask = 1
-            
-            print(attack_dmg,defense_dmg,attacker_hits,attacker_crits,defender_hits,defender_crits)
-
-            while attacker_total_dice !=0 and defender_total_dice !=0: #only need to do checks when both players have dice left
-                #attacker turn
-
-                #is attacker dead
-                if defense_dmg >= attacker_wounds:
-                    print('attacker dead')
-                    attacker_hits = 0
-                    attacker_crits = 0
-                
-                #pick action
-                if attacker_crits != 0 and defender_crits !=0 and (x & mask) == 0: #parry
-                    defender_crits -= 1
-                    attacker_crits -= 1
-                elif attacker_hits != 0 and defender_hits !=0 and (x & mask) == 0: #parry
-                    defender_hits -= 1
-                    attacker_hits -= 1
-                elif attacker_crits != 0: #attack
-                    attack_dmg += attacker['WCD']
-                    attacker_crits -= 1
-                elif attacker_hits != 0: #attack
-                    attack_dmg += attacker['WD']
-                    attacker_hits -= 1   
-                else:
-                    pass
 
 
-                print(attack_dmg,defense_dmg,attacker_hits,attacker_crits,defender_hits,defender_crits)
-                
-                #defender turn
 
-                #is defender dead
-                if attack_dmg >= defender_wounds:
-                    print('defender dead')
-                    defender_hits = 0
-                    defender_crits = 0
-                
-                #pick action
-                if defender_crits != 0 and attacker_crits !=0 and (y & mask) == 0: #parry
-                    defender_crits -= 1
-                    attacker_crits -= 1
-                elif defender_hits != 0 and attacker_hits !=0 and (y & mask) == 0: #parry
-                    defender_hits -= 1
-                    attacker_hits -= 1
-                elif defender_crits != 0: #attack
-                    defense_dmg += defender['WCD']
-                    defender_crits -= 1
-                elif defender_hits != 0: #attack
-                    defense_dmg += defender['WD']
-                    defender_hits -= 1  
-                else:
-                    pass
 
-                print(attack_dmg,defense_dmg,attacker_hits,attacker_crits,defender_hits,defender_crits)
-                    
-                #update leftover dice totals and move resolution round mask for bitwise compare
-                attacker_total_dice = attacker_hits + attacker_crits
-                defender_total_dice = defender_hits + defender_crits
-                mask = mask * 2
-
-                
-
-            
-
-            #add up leftover dice
-            attack_dmg += (attacker_hits * attacker['WD'])
-            attack_dmg += (attacker_crits * attacker['WCD'])
-            attacker_hits = 0
-            attacker_crits = 0
-            defense_dmg += (defender_hits * defender['WD'])
-            defense_dmg += (defender_crits * defender['WCD'])
-            defender_hits = 0
-            defender_crits = 0
-            damage_totals[damage_pos_counter] = (attack_dmg,defense_dmg)
-            damage_pos_counter += 1
-            print(attack_dmg,defense_dmg,attacker_hits,attacker_crits,defender_hits,defender_crits)
-
-    print(damage_totals)
-
-    # Remove duplicate values in dictionary
-    # Using dictionary comprehension
-    temp = {val : key for key, val in damage_totals.items()}
-    res = {val : key for key, val in temp.items()}
     
-    # printing result 
-    print()
-    print(str(res)) 
+
 
 
 
@@ -426,11 +657,11 @@ def prob_ranged_death(attacker, defender, trials = 10000):
 
 
 guardsmen1 = {'M':6 , 'APL':2, 'GA':2, 'BA':4, 'WA':3, 'BS':4, 'WS':4, 'BD':2, 'BCD':3, 'WD':2, 'WCD':3, 'DF':3, 'SV':5, 'W':7}
-kommando = {'M':6 , 'APL':2, 'GA':1, 'BA':5, 'WA':3, 'BS':4, 'WS':3, 'BD':3, 'BCD':4, 'WD':3, 'WCD':4, 'DF':3, 'SV':5, 'W':10}
+kommando = {'M':6 , 'APL':2, 'GA':1, 'BA':5, 'WA':3, 'BS':4, 'WS':3, 'BD':3, 'BCD':4, 'WD':2, 'WCD':3, 'DF':3, 'SV':5, 'W':10}
 
 #dice_check(100000,4)
 
-melee_phase(kommando, guardsmen1)
+melee_phase_user_picks(kommando, guardsmen1)
 
 '''
 data = prob_ranged_death(kommando, guardsmen1)
